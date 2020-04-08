c633b8eca0ff8c2b13012ee113a5995b362895ca
fabianomg <fabianodasilva496@gmail.com>
2020-04-08 14:26:34 -0300

    teste

--- app/Controllers/Http/MongoController.js
+++ app/Controllers/Http/MongoController.js
@@ -9,9 +9,9 @@ class MongoController {
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0",
-        Accept: "application/json"
-      }
-    }).then(async response => {
+        Accept: "application/json",
+      },
+    }).then(async (response) => {
       let key = response.data;
 
       setTimeout(() => {
@@ -20,16 +20,18 @@ class MongoController {
 
           for (const item of result) {
             let d = item.split("#");
-            Redis.srem(id + "listcards", 0, d[0]);
+            if (d[1] == "false") {
+              Redis.srem(id + "listcards", 0, d[0]);
+            }
           }
           Redis.smembers(id + "listcards", async (err, t) => {
             let r;
 
-            result != '' ? (r = result.length) : (r = 0);
+            result != "" ? (r = result.length) : (r = 0);
 
             let total = t.length + result.length;
             Menssagem.interacao02(id, total, r, result);
-            if (t == '') {
+            if (t == "") {
               Menssagem.stop(id);
             }
           });
